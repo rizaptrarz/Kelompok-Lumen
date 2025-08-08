b2e4c730d72bbb879fd491bae152b084c61dd222
APhishh <151861180+APhishh@users.noreply.github.com>
2025-08-08 15:13:13 +0700

    Jemuran mechanics basics

---
+++ scripts/jemuran.gd
@@ -0,0 +1,65 @@
+extends Node2D
+
+@onready var interactable: Area2D = $interactable
+@onready var sprite: Sprite2D = $sprite
+
+@export var texture_empty: Texture2D
+@export var texture_j3baju: Texture2D
+
+@export var max_health: int = 100
+var current_health: float = 100
+var is_taking_damage := false
+var is_dead := false
+
+@export var event_chance_per_second := 0.5
+@export var damage_per_second := 10
+@export var cooldown : float = 5 
+var cooldownTimer : float = cooldown
+
+var random_event_timer := 0.0
+signal JemuranDestroyed
+func _ready() -> void:
+	sprite.show()
+	interactable.interact = _on_interact
+	current_health = max_health
+
+func _on_interact():
+	is_taking_damage = false
+	cooldownTimer = cooldown
+	if is_dead:
+		return
+	if sprite.texture == texture_empty:
+		sprite.texture = texture_j3baju
+	else:
+		sprite.texture = texture_empty
+		
+
+
+func _process(delta: float) -> void:
+
+	random_event_timer += delta
+	if random_event_timer >= 1.0 and not is_taking_damage:
+		random_event_timer = 0.0
+		if randf() <= event_chance_per_second and not is_dead and cooldownTimer <= 0:
+			is_taking_damage = true
+			print("Something is attacking!")
+
+	if is_taking_damage:
+		var damage := damage_per_second * delta
+		current_health -= damage
+		print("Health: " , current_health)
+
+		if current_health <= 0:
+			current_health = 0
+			is_taking_damage = false
+			_on_health_depleted()
+			
+			
+	if not is_taking_damage and cooldownTimer > 0:
+		cooldownTimer -= delta
+		print("On cooldown: ", cooldownTimer)
+	
+func _on_health_depleted():
+	is_dead = true
+	print("Dead :(")
+	JemuranDestroyed.emit()
