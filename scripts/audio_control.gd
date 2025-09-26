ae3cbfadfda712a4b02686a50864cbe82b8195e3
RidwanRamdhani <gtiridwan@gmail.com>
2025-09-26 14:20:18 +0700

    add meja and changing music

--- scripts/audio_control.gd
+++ scripts/audio_control.gd
@@ -6,8 +6,11 @@ var audio_bus_id : int
 
 func _ready() -> void:
 	audio_bus_id = AudioServer.get_bus_index(audio_bus_name)
+	value = VolumeGlobal.music_volume
+	AudioServer.set_bus_volume_db(audio_bus_id, linear_to_db(value))
 	
 
 func _on_value_changed(value: float) -> void:
 	var db : float = linear_to_db(value)
 	AudioServer.set_bus_volume_db(audio_bus_id, db)
+	VolumeGlobal.music_volume = value
---
+++ scripts/volumeGlobal.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var music_volume: float = 1.0  # default 100%
